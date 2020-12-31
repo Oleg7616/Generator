bacb52e0f950c2bf443508868f39d195e9dd72bf
Oleg Leontjev <oleg.leontjev@mail.ru>
2020-12-31 08:34:52 +0200

    init

--- init.js
+++ init.js
@@ -5,4 +5,8 @@ window.onload = function()
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
     document.getElementById('surnameOutput').innerText = initPerson.surname;
     document.getElementById('fatherNameOutput').innerText = initPerson.fatherName;
+    document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
+    document.getElementById('professionOutput').innerText = initPerson.profession;
+    document.getElementById('birthMonthOutput').innerText = initPerson.birthMonth;
+    document.getElementById('birthDateOutput').innerText = initPerson.birthDate;
 };
--- personGenerator.js
+++ personGenerator.js
@@ -27,12 +27,12 @@ const personGenerator = {
             "id_2": "Максим",
             "id_3": "Иван",
             "id_4": "Артем",
-            "id_5": "Дмитрий",
-            "id_6": "Никита",
+            "id_5": "Роман",
+            "id_6": "Борис",
             "id_7": "Михаил",
             "id_8": "Даниил",
             "id_9": "Егор",
-            "id_10": "Андрей"
+            "id_10": "Олег"
         }
     }`,
     firstNameFemaleJson: `{
@@ -50,19 +50,54 @@ const personGenerator = {
             "id_10": "Анна"
         }
     }`,
-    fatherNameMaleJson: `{
+
+    professionMaleJson: `{
         "count": 10,
         "list": {     
-            "id_1": "Александрович",
-            "id_2": "Максимович",
-            "id_3": "Иванович",
-            "id_4": "Олегович",
-            "id_5": "Дмитриевич",
-            "id_6": "Борисович",
-            "id_7": "Михайлович",
-            "id_8": "Петрович",
-            "id_9": "Сергеевич",
-            "id_10": "Андреевич"
+            "id_1": "Директор",
+            "id_2": "Врач",
+            "id_3": "Учитель",
+            "id_4": "Бармен",
+            "id_5": "Официант",
+            "id_6": "Водитель",
+            "id_7": "Предприниматель",
+            "id_8": "Програмист",
+            "id_9": "Дизайнер",
+            "id_10": "Токарь"
+        }
+    }`,
+
+    professionFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Медсестра",
+            "id_2": "Швея",
+            "id_3": "Учительница",
+            "id_4": "Секретарша",
+            "id_5": "Балерина",
+            "id_6": "Певица",
+            "id_7": "Танцовщица",
+            "id_8": "Домохозяйка",
+            "id_9": "Официантка",
+            "id_10": "Бизнесвумен"
+        }
+    }`,
+
+    birthMonthJson: `{
+        "count": 12,
+        "list": {     
+            "id_1": "Января",
+            "id_2": "Февраля",
+            "id_3": "Марта",
+            "id_4": "Апреля",
+            "id_5": "Мая",
+            "id_6": "Июня",
+            "id_7": "Июля",
+            "id_8": "Августа",
+            "id_9": "Сентября",
+            "id_10": "Октября",
+            "id_11": "Ноября",
+            "id_12": "Декабря"
         }
     }`,
 
@@ -77,32 +112,78 @@ const personGenerator = {
         return obj.list[prop];
     },
 
+    randomBirthYear: function () {
+        return this.randomIntNumber (2000, 1960);
+    },
+
+    randomBirthMonth: function () {
+        return this.randomValue(this.birthMonthJson)
+
+    },
+
+    randomBirthDate: function () {
+        if (this.person.birthMonth === `Февраля`) {
+            return this.randomIntNumber (28, 1);
+        }
+        else if (this.person.birthMonth === `Апреля`) {
+            return this.randomIntNumber (30, 1);
+        }
+        else if (this.person.birthMonth === `Июня`) {
+            return this.randomIntNumber (30, 1);
+        }
+        else if (this.person.birthMonth === `Сентября`) {
+            return this.randomIntNumber (30, 1);
+        }
+        else if (this.person.birthMonth === `Ноября`) {
+            return this.randomIntNumber (30, 1);
+        }
+        else {
+            return this.randomIntNumber (31, 1);
+        }
+
+    },
+
     randomGender: function () {
         return this.randomIntNumber () === 1 ? this.GENDER_MALE : this.GENDER_FEMALE;
 
     },
 
-    randomFirstName: function() {
-        if (this.randomGender == this.GENDER_MALE) {
+    randomFirstName: function () {
+        if (this.person.gender === this.GENDER_MALE) {
             return this.randomValue(this.firstNameMaleJson); 
-             }
-        else {
+         }
+         else {
             return this.randomValue(this.firstNameFemaleJson);
-             }
+         }
     },
 
 
-     randomSurname: function() {
+     randomSurname: function () {
+        if (this.person.gender == this.GENDER_MALE) {
+             return this.randomValue(this.surnameJson);
+          }
+          else {
+              return this.randomValue(this.surnameJson) + "a";
+          }
+        },
 
-        return this.randomValue(this.surnameJson);
-
+     randomFatherName: function () {
+        if (this.person.gender == this.GENDER_MALE) {
+             return this.randomValue(this.firstNameMaleJson) + "ович";
+        }
+        else {
+             return this.randomValue(this.firstNameMaleJson) + "овна";
+          }
     },
 
-    randomFatherName: function() {
-
-        return this.randomValue(this.fatherNameMaleJson);
-
-    },
+     randomProfession: function () {
+         if (this.person.gender === this.GENDER_MALE) {
+             return this.randomValue(this.professionMaleJson);
+         }
+         else {
+             return this.randomValue(this.professionFemaleJson);
+         }
+     },
 
 
     getPerson: function () {
@@ -110,7 +191,11 @@ const personGenerator = {
         this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
         this.person.surname = this.randomSurname();
+        this.person.birthYear = this.randomBirthYear();
         this.person.fatherName = this.randomFatherName();
+        this.person.profession = this.randomProfession();
+        this.person.birthMonth = this.randomBirthMonth();
+        this.person.birthDate = this.randomBirthDate();
         return this.person;
     }
 };
